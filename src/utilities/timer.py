2419f17f2d1e12314f3d4cf4366624f7f080e4af
Anthony Lee <anthony8lee@gmail.com>
2023-03-19 20:51:53 -0600

    Updated syntax.

--- src/utilities/timer.py
+++ src/utilities/timer.py
@@ -2,10 +2,10 @@ from time import time
 
 def time_perf(func): 
     def wrap_func(*args, **kwargs):
-        start_time = time()
+        start_time = time()*1000
         result = func(*args, **kwargs)
-        stop_time = time()
+        stop_time = time()*1000
 
-        print(f"    Function {func.__name__} executed in {(stop_time-start_time):.4f} seconds.")
+        print(f"{' '*10}Function {func.__name__} executed in {(stop_time-start_time):.4f} milliseconds.")
         return result
     return wrap_func
