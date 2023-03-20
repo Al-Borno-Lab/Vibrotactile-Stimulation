c9b9d0d274da0901905488713821ad99154f08cc
Anthony Lee <anthony8lee@gmail.com>
2023-03-20 11:29:18 -0600

    Comment out the test model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,7 +2,7 @@
 from .lifNetwork import *  # LIF Model
 
 # Testing models
-from .lifNetwork_tonyTEST import *
+# from .lifNetwork_tonyTEST import *
 
 # Utility
 from .externalStimGenerator import *
