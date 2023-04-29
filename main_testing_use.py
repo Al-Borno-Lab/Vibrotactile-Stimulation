fc4845ee5c97810a71af12f6d119999159f2c318
Anthony Lee <anthony8lee@gmail.com>
2023-04-29 17:15:08 -0600

    Added testing script

---
+++ main_testing_use.py
@@ -0,0 +1,13 @@
+## Manually import each module in
+# from src.model import lifNetwork as lif
+# lif_ = lif.LIF_Network(n_neurons=700)
+
+## Testing the sub-package init module
+import src.model as mod
+lif_ = mod.LIF_Network(n_neurons=700)
+
+print(lif_)
+
+lif_.simulate(sim_duration=7000)
+
+# lif_.spikeTrain(first_n_neurons=100)
